1c3ba28448a62bfba92358b8365fc284b1e8dd8f
cristalc12 <105386097+cristalc12@users.noreply.github.com>
2023-07-20 16:02:07 -0400

    Update puzzle.js

--- puzzle.js
+++ puzzle.js
@@ -1,16 +1,18 @@
 
-var rows = 3;
-var columns = 3;
+var rows = 4;
+var columns = 4;
 
 var currTile;
-var otherTile; //blank tile
+var blankTile; //blank tile
 
 var turns = 0;
 
 // var imgOrder = ["1", "2", "3", "4", "5", "6", "7", "8", "9"];
-var imgOrder = ["4", "2", "8", "5", "1", "6", "7", "9", "3"];
+var imgOrder = ["4", "11", "8", "10", "1", "14", "7", "13", "3","5","2","15", "12","9","6","16"];
 
 window.onload = function() {
+  
+
     for (let r=0; r < rows; r++) {
         for (let c=0; c < columns; c++) {
 
@@ -18,6 +20,9 @@ window.onload = function() {
             let tile = document.createElement("img");
             tile.id = r.toString() + "-" + c.toString();
             tile.src = imgOrder.shift() + ".jpg";
+
+            tile.addEventListener("mouseover", mouseOver);
+            tile.addEventListener("mouseout", mouseOut);
 
             //DRAG FUNCTIONALITY
             tile.addEventListener("dragstart", dragStart);  //click an image to drag
@@ -28,13 +33,30 @@ window.onload = function() {
             tile.addEventListener("dragend", dragEnd);      //after drag drop, swap the two tiles
 
             document.getElementById("board").append(tile);
+           
+            
 
         }
     }
 }
 
+
+
+function mouseOver() {
+    if (isAdjacentToBlankTile(this)) {
+        this.style.borderColor = "red";
+    }
+}
+
+function mouseOut() {
+    this.style.borderColor = "black";
+}
+
 function dragStart() {
-    currTile = this; //this refers to the img tile being dragged
+    currTile = this;
+    if (isAdjacentToBlankTile(currTile)) {
+        this.style.borderColor = "red";
+    }
 }
 
 function dragOver(e) {
@@ -46,44 +68,59 @@ function dragEnter(e) {
 }
 
 function dragLeave() {
-
+    currTile.style.borderColor = "black";
 }
 
 function dragDrop() {
-    otherTile = this; //this refers to the img tile being dropped on
+    blankTile = this;
 }
 
 function dragEnd() {
-    if (!otherTile.src.includes("3.jpg")) {
+    currTile.style.borderColor = "black";
+    if (!blankTile.src.includes("16.jpg")) {
         return;
     }
 
-    let currCoords = currTile.id.split("-"); //ex) "0-0" -> ["0", "0"]
+    let currCoords = currTile.id.split("-");
     let r = parseInt(currCoords[0]);
     let c = parseInt(currCoords[1]);
 
-    let otherCoords = otherTile.id.split("-");
+    let otherCoords = blankTile.id.split("-");
     let r2 = parseInt(otherCoords[0]);
     let c2 = parseInt(otherCoords[1]);
 
-    let moveLeft = r == r2 && c2 == c-1;
-    let moveRight = r == r2 && c2 == c+1;
+    let moveLeft = r === r2 && c2 === c - 1;
+    let moveRight = r === r2 && c2 === c + 1;
+    let moveUp = c === c2 && r2 === r - 1;
+    let moveDown = c === c2 && r2 === r + 1;
 
-    let moveUp = c == c2 && r2 == r-1;
-    let moveDown = c == c2 && r2 == r+1;
 
-    let isAdjacent = moveLeft || moveRight || moveUp || moveDown;
-
-    if (isAdjacent) {
+    if (isAdjacentToBlankTile(currCoords)) {
         let currImg = currTile.src;
-        let otherImg = otherTile.src;
+        let otherImg = blankTile.src;
 
         currTile.src = otherImg;
-        otherTile.src = currImg;
+        blankTile.src = currImg;
 
         turns += 1;
         document.getElementById("turns").innerText = turns;
     }
+}
+
+function isAdjacentToBlankTile(tile) {
+    let currCoords = tile.id.split("-");
+    let r = parseInt(currCoords[0]);
+    let c = parseInt(currCoords[1]);
+
+    let blankCoords = blankTile.id.split("-");
+    let r2 = parseInt(blankCoords[0]);
+    let c2 = parseInt(blankCoords[1]);
+
+    let moveLeft = r === r2 && c2 === c - 1;
+    let moveRight = r === r2 && c2 === c + 1;
+    let moveUp = c === c2 && r2 === r - 1;
+    let moveDown = c === c2 && r2 === r + 1;
 
 
+    return moveLeft || moveRight || moveUp || moveDown;
 }
